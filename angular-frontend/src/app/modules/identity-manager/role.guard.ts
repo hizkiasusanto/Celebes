a5f3742962c63d1422f0359869cc81a9ebebcb49
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-03 18:00:52 +0700

    #11 Create a role guard

---
+++ angular-frontend/src/app/modules/identity-manager/role.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {AuthService} from "./auth.service";
+import {catchError, map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let roles = next.data.roles as Array<string>;
+    return this.authService.getProfile().pipe(map((user: any) => {
+      if (roles.includes(user.role)) {
+        return true;
+      } else {
+        this.router.navigate(['/unauthorized']);
+        return false;
+      }
+    }), catchError(() => {
+      this.router.navigate(['/home']);
+      return of(false);
+    }))
+  }
+
+}
