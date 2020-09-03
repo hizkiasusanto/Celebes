42f4791d57349d20c280d7419b34f9cc7e5aacdc
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-04 02:11:20 +0700

    #11 Create employee details component

--- angular-frontend/src/app/modules/admin/admin-routing.module.ts
+++ angular-frontend/src/app/modules/admin/admin-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.component";
 import {EmployeesComponent} from "./tab-contents/employees/employees.component";
 import {DummyComponent} from "./tab-contents/dummy/dummy.component";
+import {EmployeeDetailComponent} from "./tab-contents/employee-detail/employee-detail.component";
 
 const routes: Routes = [
+  {path:'employee-detail',component: EmployeeDetailComponent},
   {path:'', component: AdminDashboardComponent, children: [
       {path:'employees',component: EmployeesComponent},
       {path:'dummy', component: DummyComponent},
