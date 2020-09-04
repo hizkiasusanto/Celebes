ad9c6c7e5111aa69da8b2fd5ae11e775df7abe6b
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-04 15:41:46 +0700

    #11 Build

--- angular-frontend/src/app/modules/admin/admin-routing.module.ts
+++ angular-frontend/src/app/modules/admin/admin-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdminDashboardComponent} from "./admin-dashboard/admin-dashboard.component";
 import {ListOfEmployeesComponent} from "./tab-contents/list-of-employees/list-of-employees.component";
-import {DummyComponent} from "./tab-contents/dummy/dummy.component";
 import {EmployeeDetailComponent} from "./tab-contents/employee-detail/employee-detail.component";
 
 const routes: Routes = [
   {path:'employee-details/:id',component: EmployeeDetailComponent},
   {path:'', component: AdminDashboardComponent, children: [
       {path:'list-of-employees',component: ListOfEmployeesComponent},
-      {path:'dummy', component: DummyComponent},
       {path:'',pathMatch:'full',redirectTo:'list-of-employees'}
     ]}
 ];
--- angular-frontend/src/app/modules/admin/admin.module.ts
+++ angular-frontend/src/app/modules/admin/admin.module.ts
@@ -3,13 +3,12 @@ import {SharedModule} from "../../shared/shared.module";
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { ListOfEmployeesComponent } from './tab-contents/list-of-employees/list-of-employees.component';
 import {AdminRoutingModule} from "./admin-routing.module";
-import { DummyComponent } from './tab-contents/dummy/dummy.component';
 import {EmployeeDetailComponent} from "./tab-contents/employee-detail/employee-detail.component";
 
 
 
 @NgModule({
-  declarations: [AdminDashboardComponent, ListOfEmployeesComponent, DummyComponent, EmployeeDetailComponent],
+  declarations: [AdminDashboardComponent, ListOfEmployeesComponent, EmployeeDetailComponent],
   imports: [SharedModule.forRoot(),AdminRoutingModule],
   exports: []
 })
