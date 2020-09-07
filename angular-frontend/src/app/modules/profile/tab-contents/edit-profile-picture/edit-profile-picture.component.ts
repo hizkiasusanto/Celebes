b4d434290189f9415ac682e2ddc633a5d44fd48f
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-08 03:24:07 +0700

    #9 Create an edit profile picture component

---
+++ angular-frontend/src/app/modules/profile/tab-contents/edit-profile-picture/edit-profile-picture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profile-picture',
+  templateUrl: './edit-profile-picture.component.html',
+  styleUrls: ['./edit-profile-picture.component.scss']
+})
+export class EditProfilePictureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
