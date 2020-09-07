b235e686a9366c3a01fc9822ad42d5e24dccf428
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-08 01:24:26 +0700

    #7 Link both charts to the same datepicker

--- angular-frontend/src/app/modules/expenses-manager/tab-contents/charts/daily-expenses-line-chart/daily-expenses-line-chart.component.ts
+++ angular-frontend/src/app/modules/expenses-manager/tab-contents/charts/daily-expenses-line-chart/daily-expenses-line-chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {ExpensesService} from "../../../services/expenses.service";
 import {ChartDataSets, ChartOptions} from 'chart.js';
 import {Label} from "ng2-charts";
@@ -11,11 +11,12 @@ import {RupiahPipe} from "../../../../../shared/pipes/rupiah.pipe";
   templateUrl: './daily-expenses-line-chart.component.html',
   styleUrls: ['./daily-expenses-line-chart.component.scss']
 })
-export class DailyExpensesLineChartComponent implements OnInit {
-  startDate: Date;
-  endDate: Date;
+export class DailyExpensesLineChartComponent implements OnInit, OnChanges {
+  @Input() startDate: Date;
+  @Input() endDate: Date;
+  isLoading = false;
 
-  expensesData = [];
+  expensesData: { date: Date, expense: number }[] = [];
 
   public chartData: ChartDataSets[];
   public chartLabels: Label[];
@@ -32,51 +33,40 @@ export class DailyExpensesLineChartComponent implements OnInit {
         }
       }],
       yAxes: [{
-        scaleLabel: {labelString:'Amount spent',display: true},
+        scaleLabel: {labelString: 'Amount spent', display: true},
         ticks: {
           maxTicksLimit: 10,
-          callback: function(value) {
-            return new RupiahPipe().transform(value).toString();
-          }
+          callback: (value: number|string) => new RupiahPipe().transform(value).toString()
         }
       }]
     }
   };
 
-  constructor(private expensesService: ExpensesService, private dateService: DateService, private snackBar: MatSnackBar) {
-    this.startDate = this.dateService.addDays(this.dateService.today(), -7)
-    this.endDate = this.dateService.today();
+  constructor(
+    private expensesService: ExpensesService,
+    private dateService: DateService,
+    private snackBar: MatSnackBar
+  ) {}
+
+
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    if (this.startDate && this.endDate) this.updateChartData()
   }
 
-  isLoading = false;
-
-  ngOnInit(): void {
-    this.expensesService.refreshSubject.subscribe(this.updateChartData);
-  }
-
-  listenToStartDate = ($event) => {
-    this.startDate = $event;
-    if (this.endDate !== undefined) this.updateChartData();
-  }
-
-  listenToEndDate = ($event) => {
-    this.endDate = $event;
-    if (this.startDate !== undefined) this.updateChartData();
-  }
-
-
-  updateChartData = () => {
+  updateChartData = (): void => {
     if (!this.isLoading) {
       this.isLoading = true
       this.expensesService.getDailyExpensesInDateRange(this.startDate, this.endDate)
         .subscribe((res: any) => {
           if (res.success) {
-            this.expensesData = res.expenses.sort((a, b) => {
-              return new Date(a.date).getTime() - new Date(b.date).getTime()
-            });
-
+            this.expensesData = res.expenses.sort(sortByDate);
             this.chartData = [{data: this.expensesData.map(x => x.expense)}];
-            this.chartLabels = this.expensesData.map(x => new Date(x.date).toLocaleDateString('en-GB'));
+            this.chartLabels = this.expensesData.map(x => new Date(x.date).toLocaleDateString('en-GB', {
+              day: 'numeric',
+              month: 'short'
+            }));
           } else {
             this.snackBar.open(res.msg, "", {
               panelClass: ['error-snackbar']
@@ -88,3 +78,5 @@ export class DailyExpensesLineChartComponent implements OnInit {
     }
   }
 }
+
+const sortByDate = (a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
